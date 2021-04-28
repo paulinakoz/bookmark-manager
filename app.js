13ffcf45d5fa0ba163d90af0d70653e14e4c7b08
paulinakoz <paulinakozaczek@gmail.com>
2021-04-28 16:24:14 +0200

    Fixing HTML

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const bookmarksRouter = require('./controllers/bookmarksRouter');
 
 const app = express();
--- controllers/bookmarksRouter.js
+++ controllers/bookmarksRouter.js
@@ -2,37 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const bookmarksRouter = express.Router();
 
-const { models, sequelize } = require('../models');
+const { models } = require('../models');
 
-const urlarray = [];
-const commentarray = [];
-const tagsarray = [];
+bookmarksRouter.get('/', async (req, res) => {
+    const bookmarks = await models.Bookmark.findAll();
 
-   sequelize.sync().then(async () => {
-   const bookmarks = await models.Bookmark.findAll()
-   for(let i=0; i<bookmarks.length; i++) {
-    let a = bookmarks[i].dataValues.url;
-    let b = bookmarks[i].dataValues.comment;
-    let c = bookmarks[i].dataValues.tags;
-    urlarray.push(a)
-    commentarray.push(b)
-    tagsarray.push(c)
-    }
-   }
-   )
+    const urlarray = [];
+    const commentarray = [];
+    const tagsarray = [];
+    for(let i = 0; i < bookmarks.length; i++) {
+        let url = bookmarks[i].dataValues.url;
+        let comment = bookmarks[i].dataValues.comment;
+        let tag = bookmarks[i].dataValues.tags;
+
+        urlarray.push(url);
+        commentarray.push(comment);
+        tagsarray.push(tag);
+    };
     
-   
-
-
-bookmarksRouter.get("/", (req, res) => {
-    
-
     res.render('pages/index', {
         bookmarkUrl: urlarray, 
         bookmarkComment: commentarray,
         bookmarkTags: tagsarray,
     }); 
 });
+
+bookmarksRouter.post('add', (req, res) => {
+
+})
 
 
 
