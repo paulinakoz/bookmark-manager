d57e3234bae982c4a73da40f95a59732f1a2d83a
Matthew Fanning <mfcareers@yahoo.com>
2021-04-28 16:41:34 +0100

    2

--- controllers/bookmarksRouter.js
+++ controllers/bookmarksRouter.js
@@ -6,30 +6,39 @@ const { models } = require('../models');
 
 bookmarksRouter.get('/', async (req, res) => {
     const bookmarks = await models.Bookmark.findAll();
-
-    const urlarray = [];
-    const commentarray = [];
-    const tagsarray = [];
+    const array = [];
+    
     for(let i = 0; i < bookmarks.length; i++) {
         let url = bookmarks[i].dataValues.url;
         let comment = bookmarks[i].dataValues.comment;
         let tag = bookmarks[i].dataValues.tags;
+        let id = bookmarks[i].dataValues.id;
 
-        urlarray.push(url);
-        commentarray.push(comment);
-        tagsarray.push(tag);
+        array.push([url,comment,tag, id]);
+        
     };
     
     res.render('pages/index', {
-        bookmarkUrl: urlarray, 
-        bookmarkComment: commentarray,
-        bookmarkTags: tagsarray,
+        bookmarkarray: array,
     }); 
 });
 
-bookmarksRouter.post('add', (req, res) => {
-
+bookmarksRouter.post('/add', async (req, res) => {
+    let url = req.body.url
+    let comment = req.body.comment
+    let tags = req.body.tags
+    await models.Bookmark.create({url:url, comment:comment, tags:tags})
+    res.redirect('/'); 
 })
+
+//bookmarksRouter.post('/delete', async (req, res) => {
+    //$("button").click(function() {
+  //      return idtodelete = this.id; // or alert($(this).attr('id'));
+  //  });
+ //   await models.Bookmark.destroy({idtodelete})
+ //   res.redirect('/'); 
+//})
+
 
 
 
